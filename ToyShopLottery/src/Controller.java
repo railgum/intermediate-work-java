2f9164d90a53368a68c07c2ccf3b44bed10787ea
Rail <railbolo@gmail.com>
2023-05-15 22:35:21 +0400

    merge begin

--- ToyShopLottery/src/Controller.java
+++ ToyShopLottery/src/Controller.java
@@ -4,14 +4,14 @@ import java.io.InputStreamReader;
 
 public class Controller {
         public static void updateView() throws IOException {
-            Toy toy1 = new Toy("Робот", 2, 60);
-            Toy toy2 = new Toy("Медведь", 2, 20);
-            Toy toy3 = new Toy("Утка", 2, 74);
-            Toy toy4 = new Toy("Конструктор", 2, 82);
-            Toy toy5 = new Toy("Кукла Барби", 2, 35);
-            Toy toy6 = new Toy("Лото", 2, 48);
-            Toy toy7 = new Toy("Дельфин", 2, 55);
-            Toy toy8 = new Toy("Пианино", 2, 90);
+            Toy toy1 = new Toy("Робот", 7, 60);
+            Toy toy2 = new Toy("Медведь", 4, 20);
+            Toy toy3 = new Toy("Утка", 12, 74);
+            Toy toy4 = new Toy("Конструктор", 6, 82);
+            Toy toy5 = new Toy("Кукла Барби", 9, 35);
+            Toy toy6 = new Toy("Лото", 5, 48);
+            Toy toy7 = new Toy("Дельфин", 3, 55);
+            Toy toy8 = new Toy("Пианино", 4, 90);
 
             ListToys listToys = new ListToys();
             listToys.add(toy1);
